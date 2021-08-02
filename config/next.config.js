d4963fcc29c68bf47fc690ba0ade8e2dbc0ff401
Elvis Miranda <elvismiranda924g@gmail.com>
2021-08-02 18:04:06 -0500

    try 3

--- config/next.config.js
+++ config/next.config.js
@@ -1,5 +1,5 @@
 const production = process.env.NODE_ENV === 'production';
 
 export const url = production
-  ? 'https://elvisdev-portfolio.netlify.app'
+  ? 'http://localhost:1337'
   : 'http://localhost:1337';
